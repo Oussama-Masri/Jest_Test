3db82ee6f132640ccda6e10af7f055ae29e507b1
Oussama <oussama.masri@okation.ai>
2024-11-13 15:43:11 +0200

    Add GitHub Actions CI workflow

---
+++ src/utils.test.js
@@ -0,0 +1,9 @@
+import { add, subtract } from './utils';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('subtracts 5 - 3 to equal 2', () => {
+  expect(subtract(5, 3)).toBe(2);
+});
