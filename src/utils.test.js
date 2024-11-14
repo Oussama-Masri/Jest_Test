81b688821e9af35bbc23ad3112558df3b6fe3fdc
Oussama <oussama.masri@okation.ai>
2024-11-14 10:32:24 +0200

    Add : Added the Divide Function

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,4 @@
-import { add, multiply, subtract } from "./utils";
+import { add, divide, multiply, subtract } from "./utils";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -10,4 +10,8 @@ test("subtracts 5 - 3 to equal 2", () => {
 
 test("Multiply 2 * 3 to equal 6", () => {
   expect(multiply(2, 3)).toBe(6);
+});
+
+test("Divide 6 / 3 to equal 2", () => {
+  expect(divide(6, 3)).toBe(2);
 });
